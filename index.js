dd85a09545d5df0ff503ae467231edf258fe5735
Nikhil B. Patel <90099805+Nikhil-2711@users.noreply.github.com>
2024-10-25 14:46:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
-const http = require('http');
 
 const app = express();
-const server = http.createServer(app);
-
 app.use(cors());
 
 const port = 4000;
 
 app.get('/test-host', (req, res) => {
-    // Send a response back to the client
     res.send("Hosted backend tested!!!!!");
 });
 
-server.listen(port, () => {
-    console.log(`Server listening to port ${port}`);
-});
+// Export the app
+module.exports = app;
